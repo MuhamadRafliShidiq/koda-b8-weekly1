6426c59f13f0ae32957421cc3dd8842ddca93e39
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-16 19:28:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,61 +7,56 @@ const rl = createInterface({
 // Data Menu Lazatto
 
 const menu = [
-  {
-    id: 1,
-    name: "Chicken Ori Dada",
-    harga: 13000
-  },
-  {
-    id: 2,
-    name: "Ayam CLBK Dada",
-    harga: 22000
-  },
-  {
-    id: 3,
-    name: "Paket Geprek Dada",
-    harga: 25000
-  },
-  {
-    id: 4,
-    name: "New Sadazz 1",
-    harga: 23000
-  },
-  {
-    id: 5,
-    name: "Burger Hamer",
-    harga: 13500,
-  },
-  {
-    id: 6,
-    name: "Kentang",
-    harga: 13500
-  },
-  {
-    id: 7,
-    name: "Paket Bersayap Mentai",
-    harga: 28000
-  },
-  {
-    id: 8,
-    name: "Nasi Putih",
-    harga: 7000
-  },
-  {
-    id: 9,
-    name: "Air Mineral",
-    harga: 8000
-  },
-  {
-    id: 10,
-    name: "Sambel Geprek",
-    harga: 6500
-  },
-  {
-    id: 11,
-    name: "Teh Sosro Kotak",
-    harga: 7500
-  }
+    {
+        id: 1,
+        nama: "Ayam CLBK Dada",
+        harga: 22000
+    },
+    {
+        id: 2,
+        nama: "Burger Hamer",
+        harga: 13500
+    },
+    {
+        id: 3,
+        nama: "Paket Geprek Dada",
+        harga: 25000
+    },
+    {
+        id: 4,
+        nama: "New Sadazz 1",
+        harga: 23000
+    },
+    {
+        id: 5,
+        nama: "Nasi Putih",
+        harga: 7000
+    },
+    {
+        id: 6,
+        nama: "Kentang",
+        harga: 13500
+    },
+    {
+        id: 7,
+        nama: "Sambal Geprek",
+        harga: 6500
+    },
+    {
+        id: 8,
+        nama: "Teh Botol Kotak Sosro",
+        harga: 7500
+    },
+    {
+        id: 9,
+        nama: "Paket Bersayap Mentai",
+        harga: 28000
+    },
+    {
+        id: 10,
+        nama: "Air Mineral",
+        harga: 8000
+    }
 ]
 
   
