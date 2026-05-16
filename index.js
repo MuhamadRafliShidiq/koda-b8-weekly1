d5d2ff8ff6799c94c1b36a3a8b41b52e19e11605
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-16 10:35:33 +0700

    Update index.js
    Menambahkan Variabel List menu

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
+const {createInterface} = require ("node: readline");
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+)}
 
+// Data Menu Lazatto
+
+const menu = [
+  {
+    id: 1,
+    name: "Chicken Ori Dada",
+    harga: 13000
+  },
+  {
+    id: 2,
+    name: "Ayam CLBK Dada",
+    harga: 22000
+  },
+  {
+    id: 3,
+    name: "Paket Geprek Dada",
+    harga: 25000
+  },
+  {
+    id: 4,
+    name: "New Sadazz 1",
+    harga: 23000
+  },
+  {
+    id: 5,
+    name: "Burger Hamer",
+    harga: 13500,
+  },
+  {
+    id: 6,
+    name: "Kentang",
+    harga: 13500
+  },
+  {
+    id: 7,
+    name: "Paket Bersayap Mentai",
+    harga: 28000
+  },
+  {
+    id: 8,
+    name: "Nasi Putih",
+    harga: 7000
+  },
+  {
+    id: 9,
+    name: "Air Mineral",
+    harga: 8000
+  },
+  {
+    id: 10,
+    name: "Sambel Geprek",
+    harga: 6500
+  },
+  {
+    id: 11,
+    name: "Teh Sosro Kotak",
+    harga: 7500
+  }
+]
+
+  
