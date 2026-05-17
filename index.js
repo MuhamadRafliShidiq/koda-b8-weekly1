43a588c8a2e59cc6fa9b5965c3ed7eff93cc75fc
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-17 15:41:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,58 +6,12 @@ const rl = createInterface({
 
 // Data Menu Lazatto
 
-const menu = [
-    {
-        id: 1,
-        nama: "Ayam CLBK Dada",
-        harga: 22000
-    },
-    {
-        id: 2,
-        nama: "Burger Hamer",
-        harga: 13500
-    },
-    {
-        id: 3,
-        nama: "Paket Geprek Dada",
-        harga: 25000
-    },
-    {
-        id: 4,
-        nama: "New Sadazz 1",
-        harga: 23000
-    },
-    {
-        id: 5,
-        nama: "Nasi Putih",
-        harga: 7000
-    },
-    {
-        id: 6,
-        nama: "Kentang",
-        harga: 13500
-    },
-    {
-        id: 7,
-        nama: "Sambal Geprek",
-        harga: 6500
-    },
-    {
-        id: 8,
-        nama: "Teh Botol Kotak Sosro",
-        harga: 7500
-    },
-    {
-        id: 9,
-        nama: "Paket Bersayap Mentai",
-        harga: 28000
-    },
-    {
-        id: 10,
-        nama: "Air Mineral",
-        harga: 8000
-    }
-]
+const friedChicken = [
+    { id: 1, nama: "Ayam Dada", harga: 18000 },
+    { id: 2, nama: "Ayam Paha Atas", harga: 18000 },
+    { id: 3, nama: "Ayam Paha Bawah", harga: 14000 },
+    { id: 4, nama: "Ayam Sayap", harga: 23000 }
+];
 
 // Function Tampilkan Menu
 function tampilkanMenu() {
