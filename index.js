e15a7b7e97ef808c7350e075abb44f10ea734ef7
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-17 15:43:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,29 @@ const paketGeprek = [
     { id: 4, nama: "Paket Geprek Sayap", harga: 18000 }
 ];
 
+const sideDish = [
+    { id: 1, nama: "Nasi Putih", harga: 7000 },
+    { id: 2, nama: "Kentang", harga: 5000 },
+    { id: 3, nama: "Spaghetti", harga: 5000 }
+];
+
+const minuman = [
+    { id: 1, nama: "Air Mineral", harga: 8000 },
+    { id: 2, nama: "S-Tee", harga: 7000 },
+    { id: 3, nama: "Teh Botol Kotak Sosro", harga: 7500 }
+];
+
+const menu = [
+    { id: 1, nama: "Fried Chicken" },
+    { id: 2, nama: "Geprek" },
+    { id: 3, nama: "Sadazz" },
+    { id: 4, nama: "Ayam CLBK" },
+    { id: 5, nama: "Paket Chicken" },
+    { id: 6, nama: "Paket Geprek" },
+    { id: 7, nama: "Side Dish" },
+    { id: 8, nama: "Minuman" }
+];
+
 // Function Tampilkan Menu
 function tampilkanMenu() {
     console.log("\n=================================")
